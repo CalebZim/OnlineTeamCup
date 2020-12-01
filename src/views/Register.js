0e29d5ca912e232e2181d971ce8a4bb25f410c99
Caleb Zimmerman <54634315+CalebZim@users.noreply.github.com>
2020-12-01 17:23:07 -0500

    deploy

--- src/views/Register.js
+++ src/views/Register.js
@@ -7,7 +7,8 @@ const Register = () => {
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfg8_6fT4z4oVapVBthxGHVJj5tsymuwc7AhwoHgupCS6YcYA/viewform?embedded=true"
          width="700" height="700" frameborder="0" marginheight="0" marginwidth="0" title='Registration'>
         Loading…</iframe>
-        <br /><Link to="/" className="button button-primary button-wide-mobile button-sm">
+        <br />
+        <Link to="/" className="button button-primary button-wide-mobile button-sm">
         Home
         </Link>
         </div>
